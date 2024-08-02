dca8340a1f6081f18db5116dd17210ac46c5e9c3
jgabrielleal <joaogabrieel79@gmail.com>
2024-08-01 23:03:19 -0300

    FEAT: Definindo hover dos itens do navbar do painel e responsividade

--- painel/src/layout/Navbar.tsx
+++ painel/src/layout/Navbar.tsx
@@ -14,56 +14,56 @@ import perfil from '/svgs/navbar/PERFIL.svg'
 export default function Navbar(){
     return (
         <nav className="bg-aurora h-15">
-            <div className="w-10/12 lg:w-[80rem] h-15 mx-auto">
+            <div className="w-10/12 xl:w-[80rem] h-15 mx-auto">
                 <div className="flex justify-between h-14">
                     <img src={icone} className="w-8 py-[0.78rem]" alt="icone da logomarca"/>
-                    <button className="float-right text-azul-claro" aria-label="Abrir menu de navegação">
+                    <button className="float-right text-azul-claro xl:hidden" aria-label="Abrir menu de navegação">
                         <HiOutlineMenu/>
                     </button>
                 </div>
-                <ul className="bg-aurora fixed top-0 right-0 w-44 h-screen p-4">
+                <ul className="bg-aurora xl:bg-transparent fixed xl:block top-0 right-0 w-44 xl:w-full h-screen xl:h-14 p-4 xl:flex xl:gap-4 xl:justify-center">
                     <li>
-                        <Link to="#" className="font-averta font-bold uppercase italic flex gap-1 mb-2" aria-label="ir para o dashboard">
+                        <Link to="#" className="nav-hover font-averta font-bold uppercase italic flex gap-1 mb-3" aria-label="ir para o dashboard">
                             <img src={dashboard} alt="Icone dashboard"/>Dashboard
                         </Link>
                     </li>
                     <li>
-                        <Link to="#" className="font-averta font-bold uppercase italic flex gap-1 mb-2" aria-label="ir para matérias">
+                        <Link to="#" className="nav-hover font-averta font-bold uppercase italic flex gap-1 mb-3" aria-label="ir para matérias">
                             <img src={materias} alt="Icone matérias"/>Matérias
                         </Link>
                     </li>
                     <li>
-                        <Link to="#" className="font-averta font-bold uppercase italic flex gap-1 mb-2" aria-label="ir para locução">
+                        <Link to="#" className="nav-hover font-averta font-bold uppercase italic flex gap-1 mb-3" aria-label="ir para locução">
                             <img src={locucao} alt="Icone locução"/>Locução
                         </Link>
                     </li>
                     <li>
-                        <Link to="#" className="font-averta font-bold uppercase italic flex gap-1 mb-2" aria-label="Ir para rádio">
+                        <Link to="#" className="nav-hover font-averta font-bold uppercase italic flex gap-1 mb-3" aria-label="Ir para rádio">
                             <img src={radio} alt="Icone rádio"/>Rádio
                         </Link>
                     </li>
                     <li>
-                        <Link to="#" className="font-averta font-bold uppercase italic flex gap-1 mb-2" aria-label="Ir para podcasts">
+                        <Link to="#" className="nav-hover font-averta font-bold uppercase italic flex gap-1 mb-3" aria-label="Ir para podcasts">
                             <img src={podcasts} alt="Icone podcasts"/>Podcasts
                         </Link>
                     </li>
                     <li>
-                        <Link to="#" className="font-averta font-bold uppercase italic flex gap-1 mb-2" aria-label="Ir para marketing">
+                        <Link to="#" className="nav-hover font-averta font-bold uppercase italic flex gap-1 mb-3" aria-label="Ir para marketing">
                             <img src={marketing} alt="Icone marketing"/>Marketing
                         </Link>
                     </li>
                     <li>
-                        <Link to="#" className="font-averta font-bold uppercase italic flex gap-1 mb-2" aria-label="Ir para ADM's">
+                        <Link to="#" className="nav-hover font-averta font-bold uppercase italic flex gap-1 mb-3" aria-label="Ir para ADM's">
                             <img src={adms} alt="Icone ADM's"/>ADM's
                         </Link>
                     </li>
                     <li>
-                        <Link to="#" className="font-averta font-bold uppercase italic flex gap-1 mb-2" aria-label="Ir para Log's">
+                        <Link to="#" className="nav-hover font-averta font-bold uppercase italic flex gap-1 mb-3" aria-label="Ir para Log's">
                             <img src={logs} alt="Icone log's"/>Log's
                         </Link>
                     </li>
                     <li>
-                        <Link to="#" className="font-averta font-bold uppercase italic flex gap-1 mb-2" aria-label="Ir para Perfil">
+                        <Link to="#" className="nav-hover font-averta font-bold uppercase italic flex gap-1 mb-3" aria-label="Ir para Perfil">
                             <img src={perfil} alt="Icone perfil"/>Perfil
                         </Link>
                     </li>
