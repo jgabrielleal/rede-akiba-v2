b0b40d2f5fd5fa1f9fa467c9862ba8316f3367c4
jgabrielleal <joaogabrieel79@gmail.com>
2024-08-03 23:10:53 -0300

    FIX: Corrigindo estilização do componente 'BoasVindasFallback' do painel

--- painel/src/placeholders/Dashboard/BoasVindasFallback.tsx
+++ painel/src/placeholders/Dashboard/BoasVindasFallback.tsx
@@ -1,8 +1,8 @@
 export default function BoasVindasFallback() {
     return (
-        <section className="flex justify-center items-center h-screen">
-            <div className="w-10/12 xl:w-[45rem] mt-10 border-4 border-red-300 py-2 px-4 rounded-xl text-center bg-red-100">
-                Nani? Quem é você não tenho ideia do que você está fazendo aqui!
+        <section className="flex justify-center">
+            <div className="w-10/12 xl:w-[45rem] mt-10 border-4 border-azul-claro py-2 px-4 rounded-xl text-laranja text-center text-2xl uppercase italic font-averta font-bold">
+                Nani? Quem é você? Não te conheço...
             </div>
         </section>
     );
