e9ca3b2d005140354f062dff4029aa66ad81f8b0
jgabrielleal <joaogabrieel79@gmail.com>
2024-09-09 22:54:58 -0300

    FEAT: Criando preview de imagens nos inputs de imagens para criar e editar publicação

--- painel/src/components/partials/Publicacoes/CapaDaPublicacao.tsx
+++ painel/src/components/partials/Publicacoes/CapaDaPublicacao.tsx
@@ -1,13 +1,32 @@
+import React, { useState } from 'react';
+
 export default function CapaDaPublicacao() {
+    const [imagemPreview, setImagemPreview] = useState<string | null>(null);
+
+    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImagemPreview(reader.result as string);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
     return (
         <section className="mb-3">
             <span className="mb-1 block font-averta font-bold text-laranja-claro text-lg uppercase">
                 Capa da matéria
             </span>
-            <label htmlFor="CapaDaPublicacao" className="w-full h-64 bg-aurora rounded-md flex justify-center items-center text-azul-claro text-6xl font-averta font-bold">
-                +
+            <label htmlFor="capaDaPublicacao" className={`w-full h-72 ${!imagemPreview && "bg-aurora"} rounded-md overflow-hidden flex justify-center items-center text-azul-claro text-6xl font-averta font-bold`}>
+                {imagemPreview ? (
+                    <img src={imagemPreview} alt="Capa da publicacao" className="w-full h-auto bg-aurora rounded-md object-cover" />
+                ) : (
+                    "+"
+                )}
             </label>
-            <input type="file" id="CapaDaPublicacao" className="hidden" />
+            <input type="file" id="capaDaPublicacao" className="hidden" onChange={handleImageChange} />
         </section>
     )
 }
--- painel/src/components/partials/Publicacoes/ImagemEmDestaque.tsx
+++ painel/src/components/partials/Publicacoes/ImagemEmDestaque.tsx
@@ -1,13 +1,32 @@
+import React, { useState } from 'react';
+
 export default function ImagemEmDestaque() {
+    const [imagemPreview, setImagemPreview] = useState<string | null>(null);
+
+    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImagemPreview(reader.result as string);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
     return (
         <>
             <span className="mb-1 block font-averta font-bold text-laranja-claro text-lg uppercase">
                 Imagem em destaque
             </span>
-            <label htmlFor="imagemEmDestaque" className="w-full h-72 bg-aurora rounded-md flex justify-center items-center text-azul-claro text-6xl font-averta font-bold">
-                +
+            <label htmlFor="imagemEmDestaque" className={`w-full ${!imagemPreview && "h-72 bg-aurora"} rounded-md flex justify-center items-center text-azul-claro text-6xl font-averta font-bold`}>
+                {imagemPreview ? (
+                    <img src={imagemPreview} alt="Imagem em destaque" className="w-full h-auto bg-aurora rounded-md object-cover" />
+                ) : (
+                    "+"
+                )}
             </label>
-            <input type="file" id="imagemEmDestaque" className="hidden" />
+            <input type="file" id="imagemEmDestaque" className="hidden" onChange={handleImageChange} />
         </>
-    )
+    );
 }
